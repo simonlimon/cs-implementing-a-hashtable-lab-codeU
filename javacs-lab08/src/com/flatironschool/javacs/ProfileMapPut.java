654f50a667aebf404343ec78b040f5d6b3c2f400
Simon Posada Fishman <simon.posada29@gmail.com>
2016-05-31 13:10:45 -0500

    Done.

--- javacs-lab08/src/com/flatironschool/javacs/ProfileMapPut.java
+++ javacs-lab08/src/com/flatironschool/javacs/ProfileMapPut.java
@@ -8,12 +8,12 @@ import org.jfree.data.xy.XYSeries;
 import com.flatironschool.javacs.Profiler.Timeable;
 
 public class ProfileMapPut {
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//profileHashMapPut();
+		// profileHashMapPut();
 		profileMyHashMapPut();
 	}
 
@@ -38,7 +38,7 @@ public class ProfileMapPut {
 		int endMillis = 1000;
 		runProfiler("HashMap put", timeable, startN, endMillis);
 	}
-	
+
 	/**
 	 * Characterize the run time of putting a key in MyHashMap
 	 */
@@ -60,10 +60,10 @@ public class ProfileMapPut {
 		int endMillis = 5000;
 		runProfiler("MyHashMap put", timeable, startN, endMillis);
 	}
-	
+
 	/**
 	 * Runs the profiles and displays results.
-	 * 
+	 *
 	 * @param timeable
 	 * @param startN
 	 * @param endMillis
